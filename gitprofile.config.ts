67ddaedc1401767af6e9dfb27eccec7913cd6e44
Mariano <marianoechaves@gmail.com>
2024-04-12 23:28:40 -0300

    initial info

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,220 +2,147 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'MarianoChaves', // Updated GitHub username
   },
-  /**
-   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
-   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
-   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
-   */
   base: '/',
   projects: {
     github: {
-      display: true, // Display GitHub projects?
+      display: true,
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'automatic', // Changed to manual to list specific projects
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        limit: 2, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
-      manual: {
-        // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
-      },
     },
     external: {
       header: 'My Projects',
-      // To hide the `External Projects` section, keep it empty.
-      projects: [
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-      ],
+      projects: [],
     },
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
-    description: '',
+    title: 'Portfolio of Mariano Chaves',
+    description: 'Data Scientist and Software Engineer with expertise in Computational Physics and Machine Learning.',
     imageURL: '',
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: 'mariano-echaves',
+    twitter: '',
+    mastodon: '',
     researchGate: '',
     facebook: '',
     instagram: '',
-    youtube: '', // example: 'pewdiepie'
+    youtube: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
+    medium: '@marianoechaves',
+    dev: '',
+    stackoverflow: '',
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    website: 'https://marianochaves.github.io/',
+    phone: '+55 19 99724 8395',
+    email: 'marianoechaves@gmail.com',
   },
   resume: {
-    fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+    fileUrl: 'https://github.com/MarianoChaves/marianochaves.github.io/blob/main/CV.pdf',
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Python', 'C++', 'Git', 'CI/CD', 'CUDA', 'OOP', 'Design Patterns',
+    'AWS Cloud', 'Terraform', 'Data pipelines', 'Glue', 'Big Data', 'PySpark',
+    'Machine Learning', 'Statistics', 'PyTorch', 'OpenCV',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Itaú-Unibanco Holding',
+      position: 'Data Scientist',
+      from: '2018',
+      to: '2020',
+      companyLink: 'https://www.itau.com.br/',
+    },
+    {
+      company: 'Itaú-Unibanco',
+      position: 'Model validation analyst',
+      from: 'Jan 2022',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://www.itau.com.br/',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
-    },
-  ],
-  certifications: [
-    {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
+      company: 'Wingbits',
+      position: 'Part-time Software Engineer',
+      from: 'Jan 2023',
+      to: 'Present',
+      companyLink: 'https://wingbits.com/',
     },
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Universidade Estadual de Campinas, Brazil',
+      degree: 'Doctorate in Physics',
+      from: '2018',
+      to: '2022',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
+      institution: 'Universidade Federal Fluminense, Brazil',
+      degree: 'Masters in Physics',
+      from: '2016',
+      to: '2018',
+    },
+    {
+      institution: 'Universidade Federal Fluminense, Brazil',
+      degree: 'Bachelor in Computational Physics',
       from: '2012',
-      to: '2014',
+      to: '2016',
     },
   ],
   publications: [
     {
-      title: 'Publication Title',
-      conferenceName: '',
-      journalName: 'Journal Name',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
+      title: 'Testing non-standard neutrino interactions in (anti)-electron neutrino disappearance experiments',
+      journalName: 'JHEP',
+      authors: 'Chaves, Mariano E. and de Holanda, Pedro Cunha and Peres, Orlando Luis Goulart',
+      link: 'https://link.springer.com/article/10.1007/JHEP03(2023)180',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'We search for scalar and tensor non-standard interactions using (anti)-electron neutrino disappearance in oscillation data. We found a slight preference for non-zero CP violation, coming from both tensor and scalar interactions. The preference for CP violation is led by Daya Bay low-energy data with a significance that reaches ~ 1.7σ in the global analysis (and ~ 2.1σ when considering only medium baseline reactors data) compared to the standard neutrino oscillation scenario. In some cases, our results indicate that the atmospheric mass squared difference falls outside the range allowed by accelerator long baseline experiments. This suggests the need for further investigation in future work.',
     },
     {
-      title: 'Publication Title',
-      conferenceName: 'Conference Name',
-      journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
+      title: 'Resolving the LMA-dark NSI degeneracy with coherent neutrino-nucleus scattering',
+      journalName: 'JHEP',
+      authors: 'Chaves, Mariano and Schwetz, Thomas',
+      link: 'https://link.springer.com/article/10.1007/JHEP05(2021)042',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'In the presence of non-standard neutrino interactions (NSI), a degeneracy exists in neutrino oscillation data, which involves the flipping of the octant of the mixing angle θ12 and the type of the neutrino mass ordering. In this article, we revisit the status of this degeneracy in the light of recent data on coherent elastic neutrino-nucleus scattering (CEνNS) from the COHERENT experiment. For general relative couplings to up and down quarks, the degeneracy is disfavoured at the 2σ level by the latest data but remains at a higher confidence level. We investigate the requirements of future CEνNS measurements to resolve the degeneracy with high significance. We find that a measurement involving both, electron and muon neutrino flavours and a target with a neutron-to-proton ratio close to 1 is required. For example, an experiment with a silicon target at the European Spallation Source can resolve the degeneracy at more than 4σ for arbitrary relative couplings to up and down quarks.',
     },
-  ],
-  // Display articles from your medium or dev account. (Optional)
+    {
+      title: 'Improvements on perturbative oscillation formulas including non-standard neutrino interactions',
+      journalName: 'J. Phys. G',
+      authors: 'Chaves, M. E. and Gratieri, D. R. and Peres, O. L. G.',
+      link: 'https://doi.org/10.1088/1361-6471/abae17',
+      description:
+        'We use perturbation theory to obtain neutrino oscillation probabilities, including the standard mass-mixing paradigm and non-standard neutrino interactions (NSI). The perturbation is made on the standard parameters and sin2(θ13) and on the non-diagonal NSI parameters, but keeps diagonal NSI parameters non-perturbated. We perform the calculation for the channels νμ → νe and νμ → νμ. The resulting oscillation formulas are compact and present functional structure similar to the standard oscillation (SO) case. They apply to a wide range in the allowed NSI space of parameters and include the previous results from perturbative approaches as limit cases. Also, we use the compact formulas we found to explain the origin of the degeneracies in the neutrino probabilities in terms of the invariance of amplitude and phase of oscillations. Then we determine analytically the multiple sets of combinations of SO and NSI parameters that result in oscillation probabilities identical to the SO case.',
+    }],
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
+    source: 'medium', // No blog details available
+    username: 'marianoechaves',
+    limit: 3,
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-DKC4ET776Z',
   },
-  // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
     id: '',
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
-
-    // Hides the switch in the navbar
-    // Useful if you want to support a single color mode
+    defaultTheme: 'dark',
     disableSwitch: false,
-
-    // Should use the prefers-color-scheme media-query,
-    // using user system preferences, instead of the hardcoded defaultTheme
     respectPrefersColorScheme: false,
-
-    // Display the ring in Profile picture
     displayAvatarRing: true,
-
-    // Available themes. To remove any theme, exclude from here.
     themes: [
-      'light',
-      'dark',
-      'cupcake',
-      'bumblebee',
-      'emerald',
-      'corporate',
-      'synthwave',
-      'retro',
-      'cyberpunk',
-      'valentine',
-      'halloween',
-      'garden',
-      'forest',
-      'aqua',
-      'lofi',
-      'pastel',
-      'fantasy',
-      'wireframe',
-      'black',
-      'luxury',
-      'dracula',
-      'cmyk',
-      'autumn',
-      'business',
-      'acid',
-      'lemonade',
-      'night',
-      'coffee',
-      'winter',
-      'dim',
-      'nord',
-      'sunset',
-      'procyon',
+      'light', 'dark'
     ],
 
     // Custom theme, applied to `procyon` theme
@@ -229,13 +156,6 @@ const CONFIG = {
       '--rounded-btn': '3rem',
     },
   },
-
-  // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
 
   enablePWA: true,
 };
